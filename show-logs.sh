c6e44fdba06c5446746fd56e984be428be3cf69b
Elena Boschetti <elena.boschetti@studio.unibo.it>
2024-12-03 16:22:21 +0100

    add tags to referenced Ollama images, modify logs visualization script

--- show-logs.sh
+++ show-logs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# $1 --> number of lines to show
-# $2 --> container name
-docker logs --tail $1 --follow --timestamps $2
+# $1 --> container name
+docker logs --tail 50 --follow --timestamps $1
